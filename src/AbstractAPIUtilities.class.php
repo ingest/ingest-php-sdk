f4f34c7de8f567ae27efd88b70c0a1c978ce9fbe
Stephen Smith <stephen.smith@viacomcontractor.com>
2017-05-09 16:19:53 -0300

    basic curl code

--- src/AbstractAPIUtilities.class.php
+++ src/AbstractAPIUtilities.class.php
@@ -1,6 +1,5 @@
 <?php
 
-// contain basic stuff for authorizing, connecting, sending requests, processing responses, and disconnecting from the API
 abstract class AbstractAPIUtilities
 {
   function __construct($jwt)
--- src/videos/Video.class.php
+++ src/videos/Video.class.php
@@ -14,11 +14,21 @@ class Video extends AbstractAPIUtilities
   //we'll start with these two to start
   function create($data)
   {
+    $curl = curl_init($this->apiURL . "/videos/");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array("Authorization: Bearer $this->jwt", "Accept: $this->acceptHeader", "Content-Type: application/json"));
 
+    return curl_exec();
   }
 
   function get($id)
   {
+    $curl = curl_init($this->apiURL . "/videos/" . $id);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array("Authorization: Bearer $this->jwt", "Accept: $this->acceptHeader"));
 
+    return curl_exec();
   }
 }
