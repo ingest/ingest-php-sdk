f9136a6d2cf63ca1ca8e70c4fa48270631d72077
Stephen Smith <stephen.smith@viacomcontractor.com>
2017-08-24 10:36:54 -0300

    events work

---
+++ src/events/Event.php
@@ -0,0 +1,66 @@
+<?php
+namespace IngestPHPSDK\Events;
+
+class Event extends \IngestPHPSDK\AbstractAPIUtilities
+{
+  function __construct($version, $accessToken)
+  {
+    //set high-level vars
+    parent::__construct($version, $accessToken);
+  }
+
+  function count()
+  {
+    $curl = curl_init($this->apiURL . "events");
+
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array("Authorization: Bearer $this->accessToken", "Accept: $this->acceptHeader"));
+    curl_setopt($curl, CURLOPT_HEADER, true);
+
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'HEAD');
+
+    $response = curl_exec($curl);
+
+    return $this->responseProcessor($response, $curl);
+  }
+
+  function getTypes()
+  {
+    $curl = curl_init($this->apiURL . "events/types");
+
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array("Authorization: Bearer $this->accessToken", "Accept: $this->acceptHeader"));
+    curl_setopt($curl, CURLOPT_HEADER, true);
+
+    $response = curl_exec($curl);
+
+    return $this->responseProcessor($response, $curl);
+  }
+
+  function getAll()
+  {
+    $curl = curl_init($this->apiURL . "events");
+
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array("Authorization: Bearer $this->accessToken", "Accept: $this->acceptHeader"));
+    curl_setopt($curl, CURLOPT_HEADER, true);
+
+    $response = curl_exec($curl);
+
+    return $this->responseProcessor($response, $curl);
+  }
+
+  function getById($eventId)
+  {
+    $curl = curl_init($this->apiURL . "events/{$eventId}");
+
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array("Authorization: Bearer $this->accessToken", "Accept: $this->acceptHeader"));
+    curl_setopt($curl, CURLOPT_HEADER, true);
+
+    $response = curl_exec($curl);
+
+    return $this->responseProcessor($response, $curl);
+  }
+
+}
