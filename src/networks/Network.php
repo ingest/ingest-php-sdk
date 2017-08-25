629741abc240fc3705b983f0bd9284a7f405313a
Stephen Smith <stephen.smith@viacomcontractor.com>
2017-08-25 10:23:29 -0300

    PHPDoc work

--- src/networks/Network.php
+++ src/networks/Network.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Networks are the main organizing resource in Ingest. These functions help you manage them.
+ */
+
 namespace IngestPHPSDK\Networks;
 
 class Network extends \IngestPHPSDK\AbstractAPIUtilities
@@ -10,6 +14,11 @@ class Network extends \IngestPHPSDK\AbstractAPIUtilities
     parent::__construct($version, $accessToken);
   }
 
+  /**
+   * Returns all the Networks your token has access to.
+   *
+   * @return array The API response, split into status, headers, and content.
+   */
   function getAll()
   {
     $curl = curl_init($this->apiURL . "networks");
@@ -22,6 +31,13 @@ class Network extends \IngestPHPSDK\AbstractAPIUtilities
     return $this->responseProcessor($response, $curl);
   }
 
+  /**
+   * Returns a specific Network.
+   *
+   * @param string $networkId The ID of the Network to retrieve.
+   *
+   * @return array The API response, split into status, headers, and content.
+   */
   function getById($networkId)
   {
     $curl = curl_init($this->apiURL . "networks/{$networkId}");
@@ -34,6 +50,14 @@ class Network extends \IngestPHPSDK\AbstractAPIUtilities
     return $this->responseProcessor($response, $curl);
   }
 
+  /**
+   * Allows you to update the properties of a Network.
+   *
+   * @param string $networkId The ID of the Network to update.
+   * @param array  $body      The properties to update, and what to update them to.
+   *
+   * @return array The API response, split into status, headers, and content.
+   */
   function update($networkId, $body)
   {
     $curl = curl_init($this->apiURL . "networks/{$networkId}");
@@ -49,6 +73,15 @@ class Network extends \IngestPHPSDK\AbstractAPIUtilities
     return $this->responseProcessor($response, $curl);
   }
 
+  /**
+   * Allows you to invite someone to a Network by email.
+   *
+   * @param string $networkId        The ID of the Network they are being invited to.
+   * @param string $invitedUserName  The name of the person being invited.
+   * @param string $invitedUserEmail The email address of the person being invited.
+   *
+   * @return array The API response, split into status, headers, and content.
+   */
   function inviteUser($networkId, $invitedUserName, $invitedUserEmail)
   {
     $curl = curl_init($this->apiURL . "networks/{$networkId}/invite");
@@ -64,6 +97,14 @@ class Network extends \IngestPHPSDK\AbstractAPIUtilities
     return $this->responseProcessor($response, $curl);
   }
 
+  /**
+   * Allows you to link an existing User to a Network.
+   *
+   * @param string $networkId    The ID of the Network they are being invited to.
+   * @param string $linkedUserId The ID of the User being invited.
+   *
+   * @return array The API response, split into status, headers, and content.
+   */
   function linkUser($networkId, $linkedUserId)
   {
     $curl = curl_init($this->apiURL . "networks/{$networkId}");
@@ -79,6 +120,14 @@ class Network extends \IngestPHPSDK\AbstractAPIUtilities
     return $this->responseProcessor($response, $curl);
   }
 
+  /**
+   * Allows you to unlink a User from a Network.
+   *
+   * @param string $networkId    The ID of the Network they are being unlinked from.
+   * @param string $linkedUserId The ID of the User being unlinked.
+   *
+   * @return array The API response, split into status, headers, and content.
+   */
   function unlinkUser($networkId, $unlinkedUserId)
   {
     $curl = curl_init($this->apiURL . "networks/{$networkId}");
