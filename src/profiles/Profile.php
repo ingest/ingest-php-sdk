19751abeb693e82aa243c13e9de9f32eca10580f
Stephen Smith <stephen.smith@viacomcontractor.com>
2017-08-25 09:32:20 -0300

    PHPDoc work

--- src/profiles/Profile.php
+++ src/profiles/Profile.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Profiles control the settings by which Inputs are synthesized into outputs, which are then associated to Videos.
+ */
+
 namespace IngestPHPSDK\Profiles;
 
 class Profile extends \IngestPHPSDK\AbstractAPIUtilities
@@ -9,6 +13,13 @@ class Profile extends \IngestPHPSDK\AbstractAPIUtilities
     parent::__construct($version, $accessToken);
   }
 
+  /**
+   * Returns a count of all Profiles your token has access to.
+   *
+   * @param string $filter (Optional) A filter for the Profiles that are displayed. See main API documentation for options.
+   *
+   * @return array The API response, split into status, headers, and content.
+   */
   function count($filter = null)
   {
     if($filter != null)
@@ -31,6 +42,14 @@ class Profile extends \IngestPHPSDK\AbstractAPIUtilities
     return $this->responseProcessor($response, $curl);
   }
 
+  /**
+   * Returns information about all Profiles your token has access to.
+   *
+   * @param string $filter (Optional) A filter for the Profiles that are displayed. See main API documentation for options.
+   * @param string $filter (Optional) The string by which Profile titles are searched for.
+   *
+   * @return array The API response, split into status, headers, and content.
+   */
   function getAll($filter = null, $search = null)
   {
     $queryString = "?";
@@ -61,6 +80,13 @@ class Profile extends \IngestPHPSDK\AbstractAPIUtilities
     return $this->responseProcessor($response, $curl);
   }
 
+  /**
+   * Creates a Profile.
+   *
+   * @param array $body The data required to create a Profile. Check main API documentation for details.
+   *
+   * @return array The API response, split into status, headers, and content.
+   */
   function add($body)
   {
     $curl = curl_init($this->apiURL . "encoding/profiles");
@@ -75,6 +101,13 @@ class Profile extends \IngestPHPSDK\AbstractAPIUtilities
     return $this->responseProcessor($response, $curl);
   }
 
+  /**
+   * Returns a specific Profile.
+   *
+   * @param string $profileId The ID of the Profile you wish to retrieve.
+   *
+   * @return array The API response, split into status, headers, and content.
+   */
   function getById($profileId)
   {
     $curl = curl_init($this->apiURL . "encoding/profiles/{$profileId}");
@@ -88,6 +121,14 @@ class Profile extends \IngestPHPSDK\AbstractAPIUtilities
     return $this->responseProcessor($response, $curl);
   }
 
+  /**
+   * Allows you to update the properties of a Profile.
+   *
+   * @param string $profileId The ID of the Profile to update.
+   * @param array  $body      The full Profile object, including updated properties. This is different from many resource update functions, due to the complexity of Profiles.
+   *
+   * @return array The API response, split into status, headers, and content.
+   */
   function update($profileId, $body)
   {
     $curl = curl_init($this->apiURL . "encoding/profiles/{$profileId}");
@@ -103,6 +144,13 @@ class Profile extends \IngestPHPSDK\AbstractAPIUtilities
     return $this->responseProcessor($response, $curl);
   }
 
+  /**
+   * Deletes a Profile.
+   *
+   * @param string $profileId The ID of the Profile that will be deleted.
+   *
+   * @return array The API response, split into status, headers, and content.
+   */
   function delete($profileId)
   {
     $curl = curl_init($this->apiURL . "encoding/profiles/{$profileId}");
