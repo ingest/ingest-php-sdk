75a38e4aa074ce738b433486669a9af12830757b
Stephen Smith <stephen.smith@redspace.com>
2017-09-26 11:48:22 -0300

    cleaning up readme and tests

--- tests/VideoTests.php
+++ tests/VideoTests.php
@@ -18,7 +18,7 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 use IngestPHPSDK\Videos\Video;
 
 $version = 'application/vnd.ingest.v1+json';
-$token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovLyouaW5nZXN0LmluZm8iLCJjaWQiOiJJbmdlc3REYXNoYm9hcmQiLCJleHAiOjE1MDM2NzgwMTQsImp0aSI6IjI4OGIxNDA2LTFlZDYtNDhiZi05MDA5LTdkZGI1ZmZjOTZlZCIsImlhdCI6MTUwMzU5MTYxNCwiaXNzIjoiaHR0cHM6Ly9sb2dpbi5pbmdlc3QuaW5mbyIsIm50dyI6IjQyZWJlNmFkLTYyMmQtNDY4MC05ZTMxLTU4ZmIyMDAwYjNlZCIsInNjb3BlIjp7ImFjdGlvbnMiOlsicGVyc29uYWwiLCJyZWFkX2JpbGxpbmciLCJyZWFkX2NsaWVudHMiLCJyZWFkX2V2ZW50cyIsInJlYWRfaG9va3MiLCJyZWFkX2lucHV0cyIsInJlYWRfam9icyIsInJlYWRfbGl2ZSIsInJlYWRfbmV0S2V5cyIsInJlYWRfbmV0d29ya3MiLCJyZWFkX3BsYXlsaXN0cyIsInJlYWRfcHJvZmlsZXMiLCJyZWFkX3VzZXJzIiwicmVhZF92aWRlb3MiLCJ3cml0ZV9iaWxsaW5nIiwid3JpdGVfY2xpZW50cyIsIndyaXRlX2hvb2tzIiwid3JpdGVfaW5wdXRzIiwid3JpdGVfam9icyIsIndyaXRlX2xpdmUiLCJ3cml0ZV9sb2NrZWRfcHJvZmlsZXMiLCJ3cml0ZV9uZXRLZXlzIiwid3JpdGVfbmV0d29ya3MiLCJ3cml0ZV9wbGF5bGlzdHMiLCJ3cml0ZV9wcm9maWxlcyIsIndyaXRlX3VzZXJzIiwid3JpdGVfdmlkZW9zIl19LCJzdWIiOiJlYTJlZDI1NS0zMTBjLTQ0MjYtOTg4MS1jNGZlOGZhYjRhZTEifQ.cvhn-YM-56Asq3yF4SdjEOzWd6VE-AVcnAAVxHxqjhVlE6YDlWLzyUthiAkLmQi0fAJoqXiYGv0oP3fRrJcf1SHAwai301gq1yZOKGbkB-9-N2IALsgKa_ClafPsoc06G6wIuhQ7_KbCPxqYgzyR-tnP59oqJjEkQegw8FkSLB_TFNiHPlWbrj6OG3xd0BH5KTi-u0oFNDwFHlWoofqOlXTD0bsfokGfAykX1w1FMVjiukkwoBuNDyqSjtpRIDUKtj5tp9fnJDvr3vMAEK4EmO15pZaOrMtj5O1AtLYY1FdWxyIxoxscJCmkAnJS3BeryKmqJTu4wZE_LWmdCEePew';
+$token = ' ';
 
 $video = new Video($version, $token);
 
@@ -78,14 +78,6 @@ if(isset($videoId))
     var_dump($retrievingThumbnails);
   }
 
-  $retrievingPlaylists = $video->getPlaylists($videoId);
-
-  if($retrievingPlaylists["status"] != "HTTP/1.1 200 OK")
-  {
-    echo "Playlists for Video $videoId could not be retrieved: \n";
-    var_dump($retrievingPlaylists);
-  }
-
   $retrievingVariants = $video->getVariants($videoId);
 
   if($retrievingVariants["status"] != "HTTP/1.1 200 OK")
@@ -104,11 +96,11 @@ if(isset($videoId))
 
   $deletingAVideo = $video->delete($videoId);
 
-    if($deletingAVideo["status"] != "HTTP/1.1 202 Accepted")
-    {
-      echo "Video $videoId could not be deleted: \n";
-      var_dump($deletingAVideo);
-    }
+  if($deletingAVideo["status"] != "HTTP/1.1 202 Accepted")
+  {
+    echo "Video $videoId could not be deleted: \n";
+    var_dump($deletingAVideo);
+  }
 }
 else
 {
